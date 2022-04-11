f0103971892017e7de9dc878c4efd9425c16169e
kps2838 <97150053+kps2838@users.noreply.github.com>
2022-04-11 17:41:14 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,24 +3,19 @@ $(function(){
         var scroll = $(window).scrollTop();
         var about_scroll = $('#about').offset().top;
         var works_scroll = $('#works').offset().top;
-        console.log(about_scroll );
-        if (scroll >= about_scroll && scroll < work_scroll) {
-          $("#gnb > ul > li > a").css("color" , "#252C65");
-          $("#gnb .border").css("background-color" , "#252C65");
-        }
-
         if (scroll < about_scroll) {
             $("#gnb > ul > li > a").css("color" , "#fff");
             $("#gnb .border").css("background-color" , "#fff");
           }
-          
+
+        if (scroll >= about_scroll && scroll < works_scroll) {
+          $("#gnb > ul > li > a").css("color" , "#252C65");
+          $("#gnb .border").css("background-color" , "#252C65");
+        }
+
         if (scroll >= works_scroll) {
             $("#gnb > ul > li > a").css("color" , "#fff");
             $("#gnb .border").css("background-color" , "#fff");
-          }
-          if (scroll < works_scroll) {
-            $("#gnb > ul > li > a").css("color" , "#252C65");
-            $("#gnb .border").css("background-color" , "#252C65");
           }
       })
 })
