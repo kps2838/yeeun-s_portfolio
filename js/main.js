4ff25d4ba86e1ada3f0e81aeec46533ecae708ec
kps2838 <97150053+kps2838@users.noreply.github.com>
2022-04-13 13:47:11 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(function(){
         var about_scroll = $('#about').offset().top;
         var works_scroll = $('#works').offset().top;
         var contact_scroll = $('#contact').offset().top;
+        var skill_scroll = $('.profile .license').offset().top;
 
         if (scroll < about_scroll) {
             $("#gnb > ul > li > a").css("color" , "#fff");
@@ -30,8 +31,61 @@ $(function(){
             $("#gnb .border").css("background-color" , "#252C65");
             $("#gnb > ul > li").eq(3).find('a').css('color','#ff0000')
           }
+
+          if (scroll == skill_scroll){
+            const skill = $('#skills > ul > li > .level_box .level');
+            skill.removeClass("on"),
+            void skill.outerWidth(),
+            skill.addClass("on");
+          }
       })
 
+
+      // 팝업창
+
+      $('#website .han #detail').click(function(){
+        $('.workdetail .w_detail_01').addClass('on');
+        $('#works .bg_black').addClass('on');
+      })
+
+      $('#website .sweet #detail').click(function(){
+        $('.workdetail .w_detail_02').addClass('on');
+        $('#works .bg_black').addClass('on');
+      })
+
+      $('#website .rbw #detail').click(function(){
+        $('.workdetail .w_detail_03').addClass('on');
+        $('#works .bg_black').addClass('on');
+      })
+
+      $('#website .uplus #detail').click(function(){
+        $('.workdetail .w_detail_04').addClass('on');
+        $('#works .bg_black').addClass('on');
+      })
+
+      $('#responsive .noroo #detail').click(function(){
+        $('.workdetail .r_detail_01').addClass('on');
+        $('#works .bg_black').addClass('on');
+      })
+
+      $('#responsive .roche #detail').click(function(){
+        $('.workdetail .r_detail_02').addClass('on');
+        $('#works .bg_black').addClass('on');
+      })
+
+
+      $('.workdetail .close').click(function(){
+        $('.workdetail > div').removeClass('on');
+        $('#works .bg_black').removeClass('on');
+      })
+
+      $(document).mouseup(function (e){
+        var LayerPopup = $(".workdetail > div");
+        if(LayerPopup.has(e.target).length === 0){
+          LayerPopup.removeClass("on");
+          $('#works .bg_black').removeClass('on');
+        }
+      })
 
   
 })
