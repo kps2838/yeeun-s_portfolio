0cd701495b42912213a587a9311f48b8a30c7b18
kps2838 <97150053+kps2838@users.noreply.github.com>
2022-05-19 14:35:05 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,8 +8,10 @@ $(function(){
           location.reload()}
   })
 
-  if(w >= 768){
-      // 스크롤 메뉴 효과
+
+  
+  // 메뉴 색상 변경
+    if(w >= 768){
       $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         var about_scroll = $('#about').offset().top;
@@ -111,7 +113,44 @@ $(function(){
             $("#gnb .border").css("background-color" , "#252C65");
             $("#gnb > ul > li").eq(3).find('a').css('color','#ff0000')
           }
-      })
+      });
+
+      // 섹션 스크롤
+      var scroll_section = function(){
+        $("section").each(function () {
+          $(this).on("mousewheel DOMMouseScroll", function (e) {
+            e.preventDefault();
+            var delta = 0;
+            if (!event) event = window.event;
+            if (event.wheelDelta) {
+                delta = event.wheelDelta / 120;
+                if (window.opera) delta = -delta;
+            } else if (event.detail) delta = -event.detail / 3;
+      
+            var moveTop = null;
+            if (delta < 0) {
+                if ($(this).next() != undefined) {
+                    moveTop = $(this).next().offset().top;
+                }
+            } else {
+                if ($(this).prev() != undefined) {
+                    moveTop = $(this).prev().offset().top;
+                }
+            }
+      
+            $("html,body").stop().animate({
+                scrollTop: moveTop + 'px'
+            }, {
+                duration: 100, complete: function () {
+                }
+      
+            });
+      
+          });
+        });
+      }
+      scroll_section();
+
     } else{
       $(window).scroll(function(){
         var scroll = $(window).scrollTop();
@@ -215,46 +254,12 @@ $(function(){
       })
     }
 
-    
-  var scroll_section = function(){
-    $("section").each(function () {
-      $(this).on("mousewheel DOMMouseScroll", function (e) {
-        e.preventDefault();
-        var delta = 0;
-        if (!event) event = window.event;
-        if (event.wheelDelta) {
-            delta = event.wheelDelta / 120;
-            if (window.opera) delta = -delta;
-        } else if (event.detail) delta = -event.detail / 3;
   
-        var moveTop = null;
-        if (delta < 0) {
-            if ($(this).next() != undefined) {
-                moveTop = $(this).next().offset().top;
-            }
-        } else {
-            if ($(this).prev() != undefined) {
-                moveTop = $(this).prev().offset().top;
-            }
-        }
-  
-        $("html,body").stop().animate({
-            scrollTop: moveTop + 'px'
-        }, {
-            duration: 100, complete: function () {
-            }
-  
-        });
-  
-      });
-    });
-  }
-
-  scroll_section();
 
 
 
-      // 팝업창
+
+  // 팝업창
 
       $('#website .han #detail').click(function(){
         $('.workdetail .w_detail_01').addClass('on');
@@ -286,6 +291,8 @@ $(function(){
         $('section').off("mousewheel DOMMouseScroll");
       })
 
+
+      // 팝업창 닫기
       $(document).mouseup(function (e){
         var LayerPopup = $(".workdetail > div");
         if(LayerPopup.has(e.target).length === 0){
